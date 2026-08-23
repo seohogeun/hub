2ddfb4ee08d9e0e364d967979624f32c1ba7df7c
seohogeun <shg5736@gmail.com>
2026-08-23 20:55:51 +0900

    sw

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>{self.skipWaiting();});self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});self.addEventListener('fetch',e=>{const u=new URL(e.request.url);if(u.pathname.endsWith('data.json')){return;}e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
